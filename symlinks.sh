8a1dfab9b8c1b8f3f5148e1c39f98446f4919a1e
Randall Spears <rspears690@gmail.com>
2016-09-12 15:26:50 -0400

    made symlinks.sh executable

--- symlinks.sh
+++ symlinks.sh
@@ -1,10 +1,10 @@
-ln ~/.dotfiles/bashrc ~/.bashrc
-ln ~/.dotfiles/Xresources ~/.Xresources
-ln ~/.dotfiles/conkyrc ~/.conkyrc
-ln ~/.dotfiles/i3 ~/.config/i3/config
-ln ~/.dotfiles/i3blocks ~/.config/i3blocks/config
-ln ~/.dotfiles/openbox/autostart ~/.config/openbox/autostart
-ln ~/.dotfiles/openbox/environment ~/.config/openbox/environment
-ln ~/.dotfiles/openbox/menu.xml ~/.config/openbox/menu.xml
-ln ~/.dotfiles/openbox/rc.xml ~/.config/openbox/rc.xml
+ln -s ~/.dotfiles/bashrc ~/.bashrc
+ln -s ~/.dotfiles/Xresources ~/.Xresources
+ln -s ~/.dotfiles/conkyrc ~/.conkyrc
+ln -s ~/.dotfiles/i3 ~/.config/i3/config
+ln -s ~/.dotfiles/i3blocks ~/.config/i3blocks/config
+ln -s ~/.dotfiles/openbox/autostart ~/.config/openbox/autostart
+ln -s ~/.dotfiles/openbox/environment ~/.config/openbox/environment
+ln -s ~/.dotfiles/openbox/menu.xml ~/.config/openbox/menu.xml
+ln -s ~/.dotfiles/openbox/rc.xml ~/.config/openbox/rc.xml
 
