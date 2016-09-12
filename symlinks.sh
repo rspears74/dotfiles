04a06ebe1e7990274597bcb0380b3383f99d8bc1
Randall Spears <rspears690@gmail.com>
2016-09-12 15:23:55 -0400

    add symlink script

---
+++ symlinks.sh
@@ -0,0 +1,10 @@
+ln ~/.dotfiles/bashrc ~/.bashrc
+ln ~/.dotfiles/Xresources ~/.Xresources
+ln ~/.dotfiles/conkyrc ~/.conkyrc
+ln ~/.dotfiles/i3 ~/.config/i3/config
+ln ~/.dotfiles/i3blocks ~/.config/i3blocks/config
+ln ~/.dotfiles/openbox/autostart ~/.config/openbox/autostart
+ln ~/.dotfiles/openbox/environment ~/.config/openbox/environment
+ln ~/.dotfiles/openbox/menu.xml ~/.config/openbox/menu.xml
+ln ~/.dotfiles/openbox/rc.xml ~/.config/openbox/rc.xml
+
