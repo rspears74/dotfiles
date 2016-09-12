e9bbf3d406a693f5ad89dd91db3265789661c387
Randall Spears <rspears690@gmail.com>
2016-09-12 16:05:55 -0400

    fixed sh script

--- symlinks.sh
+++ symlinks.sh
@@ -1,10 +1,10 @@
-ln -s ~/.dotfiles/bashrc ~/.bashrc
-ln -s ~/.dotfiles/Xresources ~/.Xresources
-ln -s ~/.dotfiles/conkyrc ~/.conkyrc
-ln -s ~/.dotfiles/i3 ~/.config/i3/config
-ln -s ~/.dotfiles/i3blocks ~/.config/i3blocks/config
-ln -s ~/.dotfiles/openbox/autostart ~/.config/openbox/autostart
-ln -s ~/.dotfiles/openbox/environment ~/.config/openbox/environment
-ln -s ~/.dotfiles/openbox/menu.xml ~/.config/openbox/menu.xml
-ln -s ~/.dotfiles/openbox/rc.xml ~/.config/openbox/rc.xml
+ln -s -f ~/.dotfiles/bashrc ~/.bashrc
+ln -s -f ~/.dotfiles/Xresources ~/.Xresources
+ln -s -f ~/.dotfiles/conkyrc ~/.conkyrc
+ln -s -f ~/.dotfiles/i3 ~/.config/i3/config
+ln -s -f ~/.dotfiles/i3blocks ~/.config/i3blocks/config
+ln -s -f ~/.dotfiles/openbox/autostart ~/.config/openbox/autostart
+ln -s -f ~/.dotfiles/openbox/environment ~/.config/openbox/environment
+ln -s -f ~/.dotfiles/openbox/menu.xml ~/.config/openbox/menu.xml
+ln -s -f ~/.dotfiles/openbox/rc.xml ~/.config/openbox/rc.xml
 
